836225554f79967c8cecf6171dd9eb124e86fd6b
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-06-07 14:28:08 +0000

    [start]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
   
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
