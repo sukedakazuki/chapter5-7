03094bc4b26c11933956232d9501459a1a0facca
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-06-10 09:28:01 +0000

    [fix]5-6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @comment = current_user.comments.new(comment_params)
-    @comment.book_id = @book.id
+    @comment.book_id = book.id
     @comment.save
   end
-  
+
   def destroy
-    @comment = Comment.find(params[:id]).destroy
+    @comment = Comment.find(params[:id])
     @comment.destroy
-    @book = Book.find(params[:book_id]) 
   end
 
   private
-
   def comment_params
     params.require(:comment).permit(:comment)
   end
