9ff6a06d32556c78718c72241f455e617283b1d4
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-06-10 08:27:27 +0000

    [finish]5-6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.comments.new(comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
   
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @comment = Comment.find(params[:id]).destroy
+    @comment.destroy
+    @book = Book.find(params[:book_id]) 
   end
 
   private
