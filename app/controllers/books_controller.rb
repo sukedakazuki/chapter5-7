8a9ce37d38192d61d77c6e8df49a4e07a1595f0a
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-06-12 01:00:52 +0000

    [finish]5-7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,7 +44,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body, :user_id, :star)
   end
   
   def is_matching_login_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_100639) do
+ActiveRecord::Schema.define(version: 2023_06_11_013337) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2023_06_05_100639) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "comments", force: :cascade do |t|
